0cef6481cbf057a38264a23193bc7a5c09a01fa9
ChatDil0 <91972076+ChatDil0@users.noreply.github.com>
2023-08-09 19:29:22 +0530

    Revert "first_commit"
    This reverts commit f2fd6894fcd755cec805cce7daf744b3d055bd42.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import "./App.css";
-import Header from './components/Header/Header';
-import Body from './components/Body/Body';
-import Footer from './components/Footer/Footer';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <>
-    <Header/>
-    <main className='main'>
-      <Body/>
-    </main>
-    <Footer/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
